e66e4bb9974ec91dc3da6962f3f1ca63faea0aa9
Md Saiyedul Morsalin <132201648+SaiyedulMorsalin@users.noreply.github.com>
2024-09-04 03:28:56 +0600

    changes model

--- donors/models.py
+++ donors/models.py
@@ -1,4 +1,3 @@
-import uuid
 from django.db import models
 from django.contrib.auth.models import User
 from .constants import BLOOD_GROUP, GENDER_TYPE, REQUEST_TYPE as BLOOD_REQUEST_TYPE
@@ -19,38 +18,33 @@ class DonorProfile(models.Model):
 class UserBloodRequest(models.Model):
     donor = models.ForeignKey(
         DonorProfile, related_name="d_req_profile", on_delete=models.CASCADE
-    )  # Changed 'user' to 'donor'
+    )
     blood_group = models.CharField(max_length=4, choices=BLOOD_GROUP)
     blood_request_type = models.CharField(choices=BLOOD_REQUEST_TYPE, max_length=20)
-    district = models.CharField(max_length=100, null=True, blank=True)
+    district = models.CharField(max_length=100)
     date_of_donation = models.DateField(null=True, blank=True)
     gender = models.CharField(choices=GENDER_TYPE, max_length=10)
     accepted_donor_id = models.CharField(max_length=12, null=True, blank=True)
-    details = models.TextField()
-    unique_identifier = models.UUIDField(
-        default=uuid.uuid4, editable=False, unique=True
-    )
+    details = models.CharField(max_length=255, unique=True)
     cancel = models.BooleanField(default=False)
 
     def __str__(self):
-        return f"Donor: {self.donor.user.first_name} - Request for  {self.blood_request_type} blood."
+        return f"Donor: {self.donor.user.first_name} - Request for {self.blood_request_type} blood."
 
 
 class UserBloodDonate(models.Model):
     donor = models.ForeignKey(
         DonorProfile, related_name="d_donate_profile", on_delete=models.CASCADE
-    )  # Changed 'user' to 'donor'
+    )
     blood_group = models.CharField(max_length=4, choices=BLOOD_GROUP)
-
-    district = models.CharField(max_length=100, null=True, blank=True)
+    district = models.CharField(max_length=100)
     date_of_donation = models.DateField(null=True, blank=True)
     gender = models.CharField(choices=GENDER_TYPE, max_length=10)
-    blood_request_type = models.CharField(
-        choices=BLOOD_REQUEST_TYPE, max_length=20, null=True, blank=True
-    )
-    details = models.TextField()
-    unique_identifier = models.TextField()
+    blood_request_type = models.CharField(choices=BLOOD_REQUEST_TYPE, max_length=20)
+    details = models.CharField(max_length=255, unique=True)
     cancel = models.BooleanField(default=False)
 
     def __str__(self):
-        return f"Donor: {self.donor.user.first_name} - Accepted  {self.blood_group} this blood."
+        return (
+            f"Donor: {self.donor.user.first_name} - Accepted {self.blood_group} blood."
+        )
