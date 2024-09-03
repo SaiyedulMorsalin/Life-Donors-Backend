c6de0745ddad15d4d576b73b7395bcc615437e71
Md Saiyedul Morsalin <132201648+SaiyedulMorsalin@users.noreply.github.com>
2024-09-04 03:43:32 +0600

    added confirmatioon link

--- donors/models.py
+++ donors/models.py
@@ -37,10 +37,11 @@ class UserBloodDonate(models.Model):
         DonorProfile, related_name="d_donate_profile", on_delete=models.CASCADE
     )
     blood_group = models.CharField(max_length=4, choices=BLOOD_GROUP)
+    blood_request_type = models.CharField(choices=BLOOD_REQUEST_TYPE, max_length=20)
     district = models.CharField(max_length=100)
     date_of_donation = models.DateField(null=True, blank=True)
     gender = models.CharField(choices=GENDER_TYPE, max_length=10)
-    blood_request_type = models.CharField(choices=BLOOD_REQUEST_TYPE, max_length=20)
+    approve_donor_id = models.CharField(max_length=12, null=True, blank=True)
     details = models.CharField(max_length=255, unique=True)
     cancel = models.BooleanField(default=False)
 
